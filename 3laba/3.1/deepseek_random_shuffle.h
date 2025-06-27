b1f53071cfbcd1bf347e55fac4897768995c1c26
luizsult <145179521+Luizsult@users.noreply.github.com>
2025-06-27 11:17:10 +0500

    Create deepseek_random_shuffle.h

---
+++ 3laba/3.1/deepseek_random_shuffle.h
@@ -0,0 +1,9 @@
+#ifndef RANDOM_SHUFFLE_H
+#define RANDOM_SHUFFLE_H
+
+#include <string>
+
+bool IsValidWord(const std::string& word);
+int ShuffleWordUntilMatch(const std::string& original_word);
+
+#endif  
