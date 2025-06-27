735d509a0f9cba063a51f4877451c7ee86a0d985
luizsult <145179521+Luizsult@users.noreply.github.com>
2025-06-27 11:23:58 +0500

    Delete 3laba/3.1 directory

--- 3laba/3.1/deepseek_random_shuffle.h
+++
@@ -1,9 +0,0 @@
-#ifndef RANDOM_SHUFFLE_H
-#define RANDOM_SHUFFLE_H
-
-#include <string>
-
-bool IsValidWord(const std::string& word);
-int ShuffleWordUntilMatch(const std::string& original_word);
-
-#endif  
