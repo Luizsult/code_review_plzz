4e3abb39103ea65cd8c4571078597c067dac9b55
luizsult <145179521+Luizsult@users.noreply.github.com>
2025-10-08 21:15:12 +0500

    Create main.cpp

---
+++ 2laba_red/3/main.cpp
@@ -0,0 +1,42 @@
+#include "linked_list.h"
+#include <iostream>
+
+int main() {
+    LinkedList list;
+    int n, value, M;
+
+    std::cout << "Введите количество элементов в списке: ";
+    std::cin >> n;
+    
+    std::cout << "Введите элементы списка: ";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> value;
+        list.Add(value);
+    }
+
+    std::cout << "Исходный список: ";
+    list.Print();
+
+    std::cout << "Введите значение M для вставки: ";
+    std::cin >> M;
+
+    InsertBeforeEverySecond(list, M);
+
+    std::cout << "Список после вставки: ";
+    list.Print();
+
+    Node* last_node = list.GetLastNode();
+    if (last_node != nullptr) {
+        std::cout << "Указатель на последний элемент списка: " << last_node << std::endl;
+        std::cout << "Значение последнего элемента: " << last_node->data << std::endl;
+    } else {
+        std::cout << "Список пуст." << std::endl;
+    }
+
+    list.Clear();
+
+    std::cout << "Проверка списка после очистки: ";
+    list.Print();
+
+    return 0;
+}
